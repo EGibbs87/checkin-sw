d3e845ccb8cb5e617cc5c791d70814c7e2a8fce5
Erik <flyers_rock_87@hotmail.com>
2017-04-08 00:07:32 +0000

    hotfix

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -17,6 +17,9 @@ class Trip < ActiveRecord::Base
       ci_response = agent.post('https://www.southwest.com/flight/retrieveCheckinDoc.html', { 'confirmationNumber' => self.confirmation_number, 'firstName' => self.user.first_name, 'lastName' => self.user.last_name, 'submitButton' => 'Check In' })
         
       form = ci_response.form_with(:name => "checkinOptions")
+      if form.nil?
+        form = ci_response.form_with(:name => "retrieveItinerary")
+      end
       button = form.button_with(:value => "Check In")
       
       agent.submit(form, button)
