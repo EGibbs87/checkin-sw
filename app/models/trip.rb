1b61d2954c3bfc6dd6d9867ee2b837bf0fbdaa3c
Erik <flyers_rock_87@hotmail.com>
2017-04-08 12:06:05 +0000

    add in error handler for too early of a response

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,6 @@ class Trip < ActiveRecord::Base
 	belongs_to :user
          
   def check_in
-    GetProxiesWorker.perform_async
-    sleep(9.minutes + 50.seconds)
     agent = Mechanize.new
     
     agent.robots = false
@@ -19,12 +17,16 @@ class Trip < ActiveRecord::Base
       ci_response = agent.post('https://www.southwest.com/flight/retrieveCheckinDoc.html', { 'confirmationNumber' => self.confirmation_number, 'firstName' => self.user.first_name, 'lastName' => self.user.last_name, 'submitButton' => 'Check In' })
         
       form = ci_response.form_with(:name => "checkinOptions")
-      if form.nil?
-        form = ci_response.form_with(:name => "retrieveItinerary")
-      end
       button = form.button_with(:value => "Check In")
       
-      agent.submit(form, button)
+      f_response = agent.submit(form, button)
+      
+      # force a random error if response includes "24 hours" (tried too early)
+       if f_response.content.match("24 hours")
+         puts "Too early, trying again"
+         a = nil
+         a.strftime("%Y-%m")
+       end
     rescue
       puts "proxy index #{i} failed"
       i += 1
@@ -32,5 +34,3 @@ class Trip < ActiveRecord::Base
     end
   end
 end
-
-# During DST, shift is UTC -(n-1), e.g. Eastern is UTC-5 normally, UTC-4 during DST
