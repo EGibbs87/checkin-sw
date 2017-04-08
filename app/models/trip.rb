c3e9e53d4f0bce224c192f6454037c413ec00e5b
Erik <flyers_rock_87@hotmail.com>
2017-04-08 12:17:01 +0000

    fix and prepare for texting (undid changes)

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,11 @@ class Trip < ActiveRecord::Base
 	belongs_to :user
          
   def check_in
+    GetProxiesWorker.perform_async
+    # allow testing
+    if DateTime.now > DateTime.new(2017,4,8,12,19)
+      sleep(9.minutes + 50.seconds)
+    end
     agent = Mechanize.new
     
     agent.robots = false
@@ -17,7 +22,11 @@ class Trip < ActiveRecord::Base
       ci_response = agent.post('https://www.southwest.com/flight/retrieveCheckinDoc.html', { 'confirmationNumber' => self.confirmation_number, 'firstName' => self.user.first_name, 'lastName' => self.user.last_name, 'submitButton' => 'Check In' })
         
       form = ci_response.form_with(:name => "checkinOptions")
+      if form.nil?
+        form = ci_response.form_with(:name => "retrieveItinerary")
+      end
       button = form.button_with(:value => "Check In")
+      
       
       f_response = agent.submit(form, button)
       
