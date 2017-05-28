005b42cb233067a2a658d6b06d0b60db0ead7869
Erik <flyers_rock_87@hotmail.com>
2017-05-28 15:31:57 +0000

    make code sleep for proper amount of time (until 20 seconds before checkin time)

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,12 +3,12 @@ class Trip < ActiveRecord::Base
          
   def check_in
     GetProxiesWorker.perform_async
-    # allow testing
+    # # allow testing
     # if DateTime.now > DateTime.new(2017,4,8,12,19)
     #   sleep(9.minutes + 50.seconds)
     # end
     
-    if DateTime.now.to_date == self.depart_time.to_date - 1.day
+    if DateTime.now.in_time_zone(self.depart_time_zone).to_date == self.depart_time.in_time_zone(self.depart_time_zone).to_date - 1.day
       direction = "depart"
       flight_time = self.depart_time
     else
@@ -18,6 +18,8 @@ class Trip < ActiveRecord::Base
     
     # set amount of time to sleep (to start process 20 seconds before checkin time)
     sleep_time = ((flight_time.strftime("%M").to_i - DateTime.now.strftime("%M").to_i).minutes - 20.seconds)
+    sleep(sleep_time) unless sleep_time < 0
+    
     agent = Mechanize.new
     
     agent.robots = false
